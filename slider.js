ed845c1a27f2e9acdac7428b4395ad973e429e67
mukhlisa96 <mukhlisakhonkhusanboyeva@gmail.com>
2022-06-13 16:53:26 +0500

    add: second block on main page

--- slider.js
+++ slider.js
@@ -21,25 +21,21 @@ controlOpen = document.querySelector('.control-open');
 controlClose = document.querySelector('.control-close');
 leftMenu = document.querySelector('.left-menu');
 contactInfo = document.querySelector('.burger-navbar-block-3');
+menuIcon = document.querySelector('.menu-icon');
+
 burger.addEventListener('click', () => {
 
-    // toggle nav
         nav.classList.toggle('burger-nav-active');
-// burgerNavActiveElements.style.opacity = 1;
         controlOpen.classList.toggle('pull-right');
         controlClose.classList.toggle('pull-left');
-
         leftMenu.classList.toggle('disappear');
-        // contactInfo.classList.toggle('disappear');
+
         if (contactInfo.style.animation) {
             contactInfo.style.animation = '';
            } else {
               contactInfo.style.animation = `contactInfoFade 1s ease forwards 1s`;
            }
 
-       
-
-    // animate links
         navLinks.forEach((link, index) => {
            if (link.style.animation) {
             link.style.animation = '';
@@ -48,7 +44,31 @@ burger.addEventListener('click', () => {
            }
         });
 
-        // burger animation
+        burger.classList.toggle('toggle');
+
+    });
+
+menuIcon.addEventListener('click', () => {
+
+        nav.classList.toggle('burger-nav-active');
+        controlOpen.classList.toggle('pull-right');
+        controlClose.classList.toggle('pull-left');
+        leftMenu.classList.toggle('disappear');
+
+        if (contactInfo.style.animation) {
+            contactInfo.style.animation = '';
+           } else {
+              contactInfo.style.animation = `contactInfoFade 1s ease forwards 1s`;
+           }
+
+        navLinks.forEach((link, index) => {
+           if (link.style.animation) {
+            link.style.animation = '';
+           } else {
+             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.6}s`;
+           }
+        });
+
         burger.classList.toggle('toggle');
 
     });
