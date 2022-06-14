bf5c20bc9fab453b1f70ba6588c754048019bd42
mukhlisa96 <mukhlisakhonkhusanboyeva@gmail.com>
2022-06-14 15:02:38 +0500

    add: camp img and plus hover effects

--- slider.js
+++ slider.js
@@ -1,5 +1,18 @@
 
 const centerblockWrapper = document.querySelector('.center-block-wrapper');
+let plusContainer = document.querySelector('.plus-container');
+var slideIndex = 0;
+
+let burger = document.querySelector('.burger');
+const nav = document.querySelector('.burger-navbar');
+
+secondBlock = document.querySelector('.lang-block');
+
+// let burgerToggle = document.querySelector('.burger .toggle');
+
+
+
+
 document.querySelectorAll('.menu-link').forEach(function(indicator, ind){
     indicator.addEventListener('click', function(){
         if(document.querySelector('.menu-link.active')){
@@ -7,13 +20,34 @@ document.querySelectorAll('.menu-link').forEach(function(indicator, ind){
         }
         indicator.classList.add('active');
         centerblockWrapper.style.transform = 'translateY('+ (ind) * -20 + '%)';
+        slideIndex = ind;
+
+        if (slideIndex==1) {
+            plusContainer.classList.add('show');
+            secondBlock.style.zIndex = -1;
+
+            burger.addEventListener('click', () => {
+                plusContainer.classList.toggle('show');
+            });
+            
+            menuIcon.addEventListener('click', () => {
+                plusContainer.classList.toggle('show');
+            });
+
+        }
+        else{
+             plusContainer.classList.remove('show');
+             secondBlock.style.zIndex = 40;
+
+        }
+
     });
 });
 
 // burger nav animation
 const navSlide = () => {
-let burger = document.querySelector('.burger');
-const nav = document.querySelector('.burger-navbar');
+// let burger = document.querySelector('.burger');
+// const nav = document.querySelector('.burger-navbar');
 const navLinks = document.querySelectorAll('.burger-menu-links li');
 
 
@@ -26,6 +60,7 @@ menuIcon = document.querySelector('.menu-icon');
 burger.addEventListener('click', () => {
 
         nav.classList.toggle('burger-nav-active');
+
         controlOpen.classList.toggle('pull-right');
         controlClose.classList.toggle('pull-left');
         leftMenu.classList.toggle('disappear');
@@ -71,8 +106,21 @@ menuIcon.addEventListener('click', () => {
 
         burger.classList.toggle('toggle');
 
-    });
-
-   
+    });     
 }
 navSlide();
+
+
+// map plus icons
+let plus = document.querySelector('.plus-1');
+let secondSlide = document.querySelector('activity-block');
+plus.addEventListener('click', () => {
+    // campImg.classList.add('show-campImg');
+
+    secondSlide.style.display = 'none';
+});
+
+
+
+
+
